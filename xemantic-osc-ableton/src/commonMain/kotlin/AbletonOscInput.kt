ecef3e6dd5327ca30b2fa8439bf289fdcaa34a86
Kazik Pogoda <morisil@xemantic.com>
2023-08-29 09:18:00 +0200

    new AbletonOscInput

---
+++ xemantic-osc-ableton/src/commonMain/kotlin/AbletonOscInput.kt
@@ -0,0 +1,158 @@
+/*
+ * xemantic-osc - Kotlin idiomatic and multiplatform OSC protocol support
+ * Copyright (C) 2023 Kazimierz Pogoda
+ *
+ * This file is part of xemantic-osc.
+ *
+ * xemantic-osc is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xemantic-osc is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with xemantic-osc.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.xemantic.osc.ableton
+
+import com.xemantic.osc.OscInput
+import com.xemantic.osc.OscMessage
+import com.xemantic.osc.OscPeer
+import kotlinx.coroutines.flow.*
+import com.xemantic.osc.route
+import io.github.oshai.kotlinlogging.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
+/**
+ * Routes `/Note*` and `/Velocity*` addresses for incoming
+ * OSC messages describing [AbletonNote]s sent out by Ableton's
+ * Max for Live OSC plugin.
+ *
+ * Note: The associated [OscInput] has to be created
+ * with a single-threaded coroutine dispatcher to guarantee
+ * that received [OscMessage]s are not reordered:
+ *
+ * ```
+ * val input = OscInput(
+ *   dispatcher = newSingleThreadContext("note-listener")
+ * ) {
+ *   routeAbletonOscNotes()
+ * }
+ * ```
+ *
+ * @param addressBase the OSC address base starting with `/`,
+ *          e.g. `/ableton`, defaults to empty string.
+ * @see unrouteAbletonOscNotes
+ */
+public fun OscInput.routeAbletonOscNotes(
+  addressBase: String = ""
+) {
+
+  val noteAddressBase = "$addressBase/Note"
+  val velocityAddressBase = "$addressBase/Velocity"
+
+  logger.info {
+    "Routing incoming Ableton OSC notes to " +
+        "$noteAddressBase* and $velocityAddressBase*"
+  }
+
+  route<Int>(
+    address = "$noteAddressBase*",
+    matchAddress = { it.startsWith(noteAddressBase) },
+  )
+
+  route<Int>(
+    address = "$velocityAddressBase*",
+    matchAddress = { it.startsWith(velocityAddressBase) },
+  )
+
+}
+
+/**
+ * Transforms the flow of [OscMessage]s to the flow
+ * of [AbletonNote]s.
+ *
+ * Note: this [Flow] should be [Flow.collect]ed only on a
+ * single-threaded coroutine, because non-concurrent map
+ * is used internally to correlate received `/Note*` and
+ * `/Velocity*` messages.
+ *
+ * @param addressBase the OSC address base starting with `/`,
+ *          e.g. `/ableton`, defaults to empty string.
+ */
+public fun Flow<OscMessage<*>>.toAbletonNotes(
+  addressBase: String = "",
+): Flow<AbletonNote> {
+  val peerNotesMap = mutableMapOf<OscPeer, MutableMap<Int, Int>>()
+  val noteAddressBase = "$addressBase/Note"
+  val velocityAddressBase = "$addressBase/Velocity"
+  return transform { message ->
+    if (message.address.startsWith(noteAddressBase)) {
+      val note = message.value as Int
+      val polyphonyIndex = message.address.removePrefix(
+        noteAddressBase
+      ).toInt()
+      val polyphonyNotesMap = peerNotesMap.getOrPut(message.peer) {
+        mutableMapOf()
+      }
+      polyphonyNotesMap[polyphonyIndex] = note
+    } else if (message.address.startsWith(velocityAddressBase)) {
+      val polyphonyIndex = message.address.removePrefix(
+        velocityAddressBase
+      ).toInt()
+      val polyphonyNotesMap = peerNotesMap[message.peer]
+      if (polyphonyNotesMap != null) {
+        val key = polyphonyNotesMap.remove(polyphonyIndex)
+        if (key != null) {
+          val abletonNote = AbletonNote(
+            peer = message.peer,
+            key = key,
+            velocity = message.value as Int,
+            polyphonyIndex = polyphonyIndex
+          )
+          logger.trace { "Received: $abletonNote" }
+          emit(abletonNote)
+        } else {
+          logger.warn {
+            "Ignoring: received ${message.address}=${message.value}, " +
+                "but no matching prior note key received"
+          }
+        }
+      } else {
+        logger.warn {
+          "Ignoring: received ${message.address}=${message.value}, " +
+              "but no matching prior note received from this peer: ${message.peer}"
+        }
+      }
+    }
+  }
+}
+
+/**
+ * Unroutes `/Note*` and `/Velocity*` OSC addresses.
+ *
+ * @param addressBase the OSC address base starting with `/`,
+ *          e.g. `/ableton`, defaults to empty string.
+ */
+public fun OscInput.unrouteAbletonOscNotes(
+  addressBase: String = ""
+) {
+
+  val noteAddressBase = "$addressBase/Note"
+  val velocityAddressBase = "$addressBase/Velocity"
+
+  logger.info {
+    "Unrouting incoming Ableton OSC notes to " +
+        "$noteAddressBase* and $velocityAddressBase*"
+  }
+
+  unroute(
+    "$noteAddressBase*",
+    "$velocityAddressBase*"
+  )
+
+}
