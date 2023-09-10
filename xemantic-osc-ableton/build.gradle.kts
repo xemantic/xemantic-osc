329510e88486d403d6c219f294d5af551ad2fcec
Kazik Pogoda <morisil@xemantic.com>
2023-09-10 17:40:52 +0200

    ableton module build updated

---
+++ xemantic-osc-ableton/build.gradle.kts
@@ -0,0 +1,92 @@
+/*
+ * xemantic-osc - Kotlin idiomatic and multiplatform OSC protocol support
+ * Copyright (C) 2023 Kazimierz Pogoda
+ *
+ * This file is part of xemantic-osc.
+ *
+ * xemantic-osc is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xemantic-osc is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with xemantic-osc.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+plugins {
+  alias(libs.plugins.kotlin.multiplatform)
+  alias(libs.plugins.dokka)
+  `maven-publish`
+}
+
+kotlin {
+
+  explicitApi()
+
+  jvm {
+    testRuns["test"].executionTask.configure {
+      useJUnitPlatform()
+    }
+  }
+
+  js(IR) {
+    browser {}
+  }
+
+  val hostOs = System.getProperty("os.name")
+  val isMingwX64 = hostOs.startsWith("Windows")
+  @Suppress("UNUSED_VARIABLE")
+  val nativeTarget = when {
+    hostOs == "Mac OS X" -> macosX64("native")
+    hostOs == "Linux" -> linuxX64("native")
+    isMingwX64 -> mingwX64("native")
+    else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+  }
+
+  sourceSets {
+
+    all {
+      languageSettings {
+        languageVersion = libs.versions.kotlinLanguageVersion.get()
+        optIn("kotlin.ExperimentalStdlibApi," +
+            "kotlinx.coroutines.ExperimentalCoroutinesApi," +
+            "kotlinx.coroutines.DelicateCoroutinesApi"
+        )
+      }
+    }
+
+    val commonMain by getting {
+      dependencies {
+        implementation(project(":xemantic-osc-api"))
+        implementation(libs.kotlin.logging)
+      }
+    }
+
+    val commonTest by getting {
+      dependencies {
+        implementation(project(":xemantic-osc-test"))
+        implementation(libs.kotest.assertions.core)
+      }
+    }
+
+    val jvmMain by getting {
+      dependencies {
+        runtimeOnly(libs.log4j.slf4j2)
+        runtimeOnly(libs.log4j.core)
+        runtimeOnly(libs.jackson.databind)
+        runtimeOnly(libs.jackson.json)
+      }
+    }
+
+    val jvmTest by getting {
+      dependencies {
+        implementation(libs.mockk)
+      }
+    }
+
+  }
+
+}
