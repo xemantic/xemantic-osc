cdb2cce14b0f6c9a298cad373f8381776e5443df
Kazik Pogoda <morisil@xemantic.com>
2023-09-11 20:23:45 +0200

    OscPeerTest

---
+++ xemantic-osc-api/src/commonTest/kotlin/OscPeerTest.kt
@@ -0,0 +1,35 @@
+/*
+ * xemantic-osc - Kotlin idiomatic and multiplatform OSC protocol support
+ * Copyright (C) 2023 Kazimierz Pogoda
+ *
+ * This file is part of xemantic-osc.
+ *
+ * xemantic-osc is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xemantic-osc is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with xemantic-osc.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.xemantic.osc
+
+import io.kotest.matchers.shouldBe
+import kotlin.test.Test
+
+class OscPeerTest {
+
+  @Test
+  fun shouldReturnOscPeerAsString() {
+    OscPeer(
+      hostname = "localhost",
+      port = 10042,
+      transport = "udp"
+    ).toString() shouldBe "udp://localhost:10042"
+  }
+
+}
