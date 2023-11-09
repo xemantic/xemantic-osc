ce9442444afbccdb98d86f6e8b0e592d1c79730c
Kazik Pogoda <morisil@xemantic.com>
2023-11-09 19:08:44 +0100

    master build file updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,20 +19,18 @@
 import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.net.URI
 
 plugins {
   alias(libs.plugins.dokka)
   alias(libs.plugins.kotlin.multiplatform) apply false
-  id("maven-publish")
-  alias(libs.plugins.gradle.versions.plugin)
+  alias(libs.plugins.versions)
+  `maven-publish`
 }
 
 val jvmVersion = JvmTarget.fromTarget(libs.versions.jvmTarget.get())
 
 allprojects {
-
-  group = "com.xemantic.osc"
-  version = "2.0-SNAPSHOT"
 
   repositories {
     mavenCentral()
@@ -59,14 +57,24 @@ allprojects {
 tasks {
 
   dokkaHtmlMultiModule.configure {
-    outputDirectory.set(buildDir.resolve("dokkaCustomMultiModuleOutput"))
+    outputDirectory.set(layout.buildDirectory.dir("dokkaCustomMultiModuleOutput"))
   }
 
   dependencyUpdates {
+
+    val nonStableKeywords = listOf("alpha", "beta", "rc")
+
+    fun isNonStable(
+      version: String
+    ): Boolean = nonStableKeywords.any {
+      version.lowercase().contains(it)
+    }
+
     gradleReleaseChannel = "current"
     rejectVersionIf {
       isNonStable(candidate.version) && !isNonStable(currentVersion)
     }
+
   }
 
 }
@@ -74,7 +82,6 @@ tasks {
 subprojects {
 
   apply {
-    plugin("kotlin-multiplatform")
     plugin("maven-publish")
     plugin("org.jetbrains.dokka")
   }
@@ -88,16 +95,43 @@ subprojects {
     }
   }
 
+  tasks.withType<Test>().configureEach {
+//    testLogging {
+//      events("started", "passed", "skipped", "failed", "standardOut", "standardError")
+//      exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+//      showExceptions = true
+//      showStackTraces = true
+//      showCauses = true
+//    }
+  }
+
+  publishing {
+//    publications.withType<MavenPublication> {
+//
+//      // Stub javadoc.jar artifact
+//      //artifact(javadocJar.get())
+//      artifactId = "${rootProject.name}-${project.name}"
+//    }
+//    publications {
+//      create<MavenPublication>("xemantic-osc") {
+//        from(components["kotlin"])
+//        artifactId = "${rootProject.name}-${project.name}"
+//      }
+//    }
+    repositories {
+      maven {
+        name = "GitHubPackages"
+        url = URI("https://maven.pkg.github.com/krisenchat/krisenchat-commons")
+        credentials {
+          username = System.getenv("GITHUB_ACTOR")
+          password = System.getenv("GITHUB_TOKEN")
+        }
+      }
+    }
+  }
+
 //  tasks.withType<KotlinCompile>().configureEach {
 //    kotlinOptions.jvmTarget = libs.versions.jvmTarget.get()
 //  }
 
-}
-
-private val notStableKeywords = listOf("alpha", "beta", "rc")
-
-fun isNonStable(
-  version: String
-): Boolean = notStableKeywords.any {
-  version.lowercase().contains(it)
 }
