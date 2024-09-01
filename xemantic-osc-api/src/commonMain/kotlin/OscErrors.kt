aafef0e1b6ebf0c8013bd912874f8867168f1dd7
Kazik Pogoda <morisil@xemantic.com>
2024-09-01 21:31:57 +0200

    OSCErrors separated

---
+++ xemantic-osc-api/src/commonMain/kotlin/OscErrors.kt
@@ -0,0 +1,34 @@
+/*
+ * xemantic-osc - Kotlin idiomatic and multiplatform OSC protocol support
+ * Copyright (C) 2024 Kazimierz Pogoda
+ *
+ * This file is part of xemantic-osc.
+ *
+ * xemantic-osc is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xemantic-osc is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with xemantic-osc.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.xemantic.osc
+
+public open class OscException(
+  message: String,
+  cause: Exception? = null
+) : RuntimeException(message, cause)
+
+public class OscInputException(
+  message: String,
+  cause: Exception? = null
+) : OscException(message, cause)
+
+public class OscOutputException(
+  message: String,
+  cause: Exception? = null
+) : OscException(message, cause)
