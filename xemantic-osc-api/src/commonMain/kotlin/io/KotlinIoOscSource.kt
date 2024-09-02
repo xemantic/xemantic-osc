4e891049c71b67d2384cb7e8180c3b028f321e8e
Kazik Pogoda <morisil@xemantic.com>
2024-09-02 17:45:38 +0200

    new KotlinIoOscSource.kt and test based on ktor 3.0 Buffer/Source/Sink

---
+++ xemantic-osc-api/src/commonMain/kotlin/io/KotlinIoOscSource.kt
@@ -0,0 +1,104 @@
+/*
+ * xemantic-osc - Kotlin idiomatic and multiplatform OSC protocol support
+ * Copyright (C) 2024 Kazimierz Pogoda
+ *
+ * This file is part of xemantic-osc.
+ *
+ * xemantic-osc is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xemantic-osc is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with xemantic-osc.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.xemantic.osc.io
+
+import com.xemantic.osc.OscInputException
+import com.xemantic.osc.OscTimeTag
+import com.xemantic.osc.oscPadding
+import kotlinx.io.*
+
+/**
+ * Creates a [Source] from supplied bytes.
+ * Useful for testing.
+ *
+ * @param bytes the sequence of bytes.
+ */
+public fun Source(
+  vararg bytes: Byte,
+): Source = Buffer().apply {
+  write(bytes)
+}
+
+/**
+ * Creates a [Source] from supplied chars.
+ * Useful for testing.
+ *
+ * @param chars the sequence of characters.
+ */
+public fun Source(
+  vararg chars: Char
+): Source = Buffer().apply {
+  write(
+    chars.map {
+      it.code.toByte()
+    }.toByteArray()
+  )
+}
+
+/**
+ * Removes bytes from this source interpreting them as OSC String.
+ * The OSC String should be `0`-terminated and padded up to 4 bytes.
+ *
+ * @return the OSC String.
+ */
+public fun Source.readOscString(): String {
+  val terminatorIndex = indexOf(0.toByte())
+  if (terminatorIndex == -1L) {
+    throw OscInputException(
+      "Cannot read OSC String, because byte sequence is not 0-terminated"
+    )
+  }
+  val string = readString(terminatorIndex)
+  if (terminatorIndex % 4L == 0L) {
+    skip(4)
+  } else {
+    skip(oscPadding(terminatorIndex))
+  }
+  return string
+}
+
+/**
+ * Removes 4 bytes from this source, interpreting them as OSC char.
+ *
+ * @return the OSC Char.
+ */
+public fun Source.readOscChar(): Char = readInt().toChar()
+
+/**
+ * Removes data from this source interpreting them as OSC blob.
+ * The first 4-bytes encode an [Int] describing the size of the blob.
+ *
+ * @return the OSC Blob.
+ */
+public fun Source.readOscBlob(): ByteArray {
+  val size = readInt()
+  val blob = readByteArray(size)
+  skip(oscPadding(size).toLong())
+  return blob
+}
+
+/**
+ * Removes 8 bytes from this source, interpreting them as OSC time tag.
+ *
+ * @return the OSC Time Tag.
+ */
+public fun Source.readOscTimeTag(): OscTimeTag = OscTimeTag(
+  seconds = readUInt(),
+  fraction = readUInt()
+)
---
+++ xemantic-osc-api/src/commonTest/kotlin/io/KotlinIoOscSourceTest.kt
@@ -0,0 +1,286 @@
+/*
+ * xemantic-osc - Kotlin idiomatic and multiplatform OSC protocol support
+ * Copyright (C) 2024 Kazimierz Pogoda
+ *
+ * This file is part of xemantic-osc.
+ *
+ * xemantic-osc is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xemantic-osc is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with xemantic-osc.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.xemantic.osc.io
+
+import com.xemantic.osc.OscInputException
+import com.xemantic.osc.OscTimeTag
+import com.xemantic.osc.ZERO
+import io.kotest.assertions.throwables.shouldThrowWithMessage
+import io.kotest.matchers.shouldBe
+import kotlinx.io.Buffer
+import kotlinx.io.EOFException
+import kotlin.test.Test
+
+class KotlinIoOscSourceTest {
+
+  @Test
+  fun shouldReadEmptyOscString() {
+    Source(0, 0, 0, 0).apply {
+      readOscString() shouldBe ""
+      exhausted() shouldBe true
+    }
+  }
+
+  @Test
+  fun shouldReadSingleCharacterOscString() {
+    Source('a', ZERO, ZERO, ZERO).apply {
+      readOscString() shouldBe "a"
+      exhausted() shouldBe true
+    }
+  }
+
+  @Test
+  fun shouldRead3CharacterOscString() {
+    Source('f', 'o', 'o', ZERO).apply {
+      readOscString() shouldBe  "foo"
+      exhausted() shouldBe true
+    }
+  }
+
+  @Test
+  fun shouldReadUtf8OscString() {
+    Source(-59, -68, -61, -77, -59, -126, -60, -121, 0, 0, 0, 0).apply {
+      readOscString() shouldBe "żółć"
+      exhausted() shouldBe true
+    }
+  }
+
+  @Test
+  fun shouldNotReadOscStringFromEmptySource() {
+    shouldThrowWithMessage<OscInputException>(
+      "Cannot read OSC String, because byte sequence is not 0-terminated"
+    ) {
+      Buffer().readOscString()
+    }
+  }
+
+  @Test
+  fun shouldNotReadOscStringFromSingle0Source() {
+    shouldThrowWithMessage<EOFException>(
+      "Buffer exhausted before skipping 4 bytes."
+    ) {
+      Source(0).readOscString()
+    }
+  }
+
+  @Test
+  fun shouldNotReadOscStringFromDouble0Source() {
+    shouldThrowWithMessage<EOFException>(
+      "Buffer exhausted before skipping 4 bytes."
+    ) {
+      Source(0, 0).readOscString()
+    }
+  }
+
+  @Test
+  fun shouldNotReadOscStringFromTriple0Source() {
+    shouldThrowWithMessage<EOFException>(
+      "Buffer exhausted before skipping 4 bytes."
+    ) {
+      Source(0, 0).readOscString()
+    }
+  }
+
+  @Test
+  fun shouldNotReadOscStringFromNonTerminatedAndNonPaddedSource() {
+    shouldThrowWithMessage<OscInputException>(
+      "Cannot read OSC String, because byte sequence is not 0-terminated"
+    ) {
+      Source(91).readOscString()
+    }
+  }
+
+  @Test
+  fun shouldNotReadOscStringFromNonPadded4Bytes() {
+    shouldThrowWithMessage<OscInputException>(
+      "Cannot read OSC String, because byte sequence is not 0-terminated"
+    ) {
+      Source(1, 2, 3, 4).readOscString()
+    }
+  }
+
+  @Test
+  fun shouldNotReadOscStringFromTerminated4BytesButNotPaddedEnough() {
+    shouldThrowWithMessage<EOFException>(
+      "Buffer exhausted before skipping 4 bytes."
+    ) {
+      Source(1, 2, 3, 4, 0).readOscString()
+    }
+  }
+
+  @Test
+  fun shouldNotReadOscStringFromNonPaddedUnicodeSource() {
+    shouldThrowWithMessage<EOFException>("Buffer exhausted before skipping 4 bytes.") {
+      Source(-59, -68, -61, -77, -59, -126, -60, -121, 0, 0, 0).readOscString()
+    }
+  }
+
+  @Test
+  fun shouldReadSingleOscChar() {
+    Source(ZERO, ZERO, ZERO, 'a').apply {
+      readOscChar() shouldBe 'a'
+      exhausted() shouldBe true
+    }
+  }
+
+  @Test
+  fun shouldReadPolishUtf8OscChar() {
+    Source(0, 0, 1, 5).apply {
+      readOscChar() shouldBe 'ą'
+      exhausted() shouldBe true
+    }
+  }
+
+  @Test
+  fun shouldReadMandarinUtf8OscChar() {
+    Source(0, 0, 79, 96).apply {
+      readOscChar() shouldBe '你'
+      exhausted() shouldBe true
+    }
+  }
+
+  @Test
+  fun shouldNotReadOscCharFromEmptySource() {
+    shouldThrowWithMessage<EOFException>(
+      "Buffer doesn't contain required number of bytes (size: 0, required: 4)"
+    ) {
+      Buffer().readOscChar()
+    }
+  }
+
+  @Test
+  fun shouldNotReadOscCharFrom1ByteLongSource() {
+    shouldThrowWithMessage<EOFException>(
+      "Buffer doesn't contain required number of bytes (size: 1, required: 4)"
+    ) {
+      Source(0).readOscChar()
+    }
+  }
+
+  @Test
+  fun shouldNotReadOscCharFrom2ByteLongSource() {
+    shouldThrowWithMessage<EOFException>(
+      "Buffer doesn't contain required number of bytes (size: 2, required: 4)"
+    ) {
+      Source(0, 1).readOscChar()
+    }
+  }
+
+  @Test
+  fun shouldNotReadOscCharFrom3ByteLongSource() {
+    shouldThrowWithMessage<EOFException>(
+      "Buffer doesn't contain required number of bytes (size: 3, required: 4)"
+    ) {
+      Source(0, 1, 2).readOscChar()
+    }
+  }
+
+  @Test
+  fun shouldReadOscBlob() {
+    Source(0, 0, 0, 1, 1, 0, 0, 0).apply {
+      readOscBlob() shouldBe byteArrayOf(1)
+      exhausted() shouldBe true
+    }
+  }
+
+  @Test
+  fun shouldReadBiggerOscBlob() {
+    Source(0, 0, 0, 2, 1, 2, 0, 0).apply {
+      readOscBlob() shouldBe byteArrayOf(1, 2)
+      exhausted() shouldBe true
+    }
+  }
+
+  @Test
+  fun shouldNotReadOscBlobFromEmptySource() {
+    shouldThrowWithMessage<EOFException>(
+      "Buffer doesn't contain required number of bytes (size: 0, required: 4)"
+    ) {
+      Buffer().readOscBlob()
+    }
+  }
+
+  @Test
+  fun shouldNotReadOscBlobIfSizeSpecificationIsTooShort() {
+    shouldThrowWithMessage<EOFException>(
+      "Buffer doesn't contain required number of bytes (size: 1, required: 4)"
+    ) {
+      Source(1).readOscBlob()
+    }
+  }
+
+  @Test
+  fun shouldNotReadOscBlobFromNonPaddedData() {
+    shouldThrowWithMessage<EOFException>(
+      "Buffer exhausted before skipping 3 bytes."
+    ) {
+      Source(0, 0, 0, 1, 1).readOscBlob()
+    }
+  }
+
+  @Test
+  fun shouldReadImmediateTimeTag() {
+    Source(
+      0, 0, 0, 0, 0, 0, 0, 1
+    ).apply {
+      readOscTimeTag() shouldBe OscTimeTag.IMMEDIATE
+      exhausted() shouldBe true
+    }
+  }
+
+  @Test
+  fun shouldReadSpecificTimeTag() {
+    Source(
+      0, 0, 0, 1, 0, 0, 0, 2
+    ).apply {
+      readOscTimeTag() shouldBe OscTimeTag(1u, 2u)
+      exhausted() shouldBe true
+    }
+  }
+
+  @Test
+  fun shouldNotReadOscTimeTagFromEmptySource() {
+    shouldThrowWithMessage<EOFException>(
+      "Buffer doesn't contain required number of bytes (size: 0, required: 4)"
+    ) {
+      Buffer().readOscTimeTag()
+    }
+  }
+
+  @Test
+  fun shouldNotReadOscTimeTagFromInsufficientSource() {
+    // size not fully specified
+    shouldThrowWithMessage<EOFException>(
+      "Buffer doesn't contain required number of bytes (size: 1, required: 4)"
+    ) {
+      Source(1).readOscTimeTag()
+    }
+  }
+
+  @Test
+  fun shouldNotReadOscTimeTagFromInsufficientSecondIntSource() {
+    // data not padded
+    shouldThrowWithMessage<EOFException>(
+      "Buffer doesn't contain required number of bytes (size: 1, required: 4)"
+    ) {
+      Source(0, 0, 0, 1, 1).readOscTimeTag()
+    }
+  }
+
+}
