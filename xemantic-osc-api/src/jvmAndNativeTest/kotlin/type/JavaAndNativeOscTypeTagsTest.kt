c2fa5834a01c9f1daf28fdc24cdd27679d1acdd3
Kazik Pogoda <morisil@xemantic.com>
2024-09-06 15:52:15 +0200

    add JavaAndNativeOscTypeTagsTest

---
+++ xemantic-osc-api/src/jvmAndNativeTest/kotlin/type/JavaAndNativeOscTypeTagsTest.kt
@@ -0,0 +1,36 @@
+/*
+ * xemantic-osc - Kotlin idiomatic and multiplatform OSC protocol support
+ * Copyright (C) 2024 Kazimierz Pogoda
+ *
+ * This file is part of xemantic-osc.
+ *
+ * xemantic-osc is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xemantic-osc is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with xemantic-osc.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.xemantic.osc.type
+
+import kotlin.test.Test
+import io.kotest.matchers.shouldBe
+
+class JavaAndNativeOscTypeTagsTest {
+
+  @Test
+  fun floatOscTypeTagShouldBeF() {
+    3.14f.oscTypeTag shouldBe "f"
+  }
+
+  @Test
+  fun longOscTypeTagShouldBeH() {
+    123456789L.oscTypeTag shouldBe "h"
+  }
+
+}
